c5563f53cf5775b2ac18909253b839876bc27dea
kstuglik <krzy_s12@wp.pl>
2019-05-06 23:03:32 +0200

    it's work

--- bucketsIF.cpp
+++ bucketsIF.cpp
@@ -1,3 +1,6 @@
+//g++ -fopenmp -Wall bucketsIF.cpp -o bif
+// ./bif numberOfPoints numberOfBuckets rangeOfNumbers numberOfThreads textScalable chunkSize
+
 #include<iostream>
 #include <stdio.h>
 #include <omp.h>
@@ -32,9 +35,9 @@ void print_vector(std::vector<int> const &input){
 
 int main(int argc, const char * argv[]) {
     
-    if (argc != 6) {
+    if (argc != 7) {
 		cout<< "Provided: " << argc << " arguments"<<endl;
-        cout<< "Please provide 5 arguments [numberOfPoints numberOfBuckets rangeOfNumbers numberOfThreads scalable]" << endl;
+        cout<< "Please provide 7 arguments [numberOfPoints numberOfBuckets rangeOfNumbers numberOfThreads textScalable chunkSize]" << endl;
         return -1;
     }
     
@@ -54,57 +57,64 @@ int main(int argc, const char * argv[]) {
     for(int i=0; i<numberOfBuckets; i++) {
         buckets[i] = new vector<int>();
     }
-    
-    omp_lock_t writeLock;
-    omp_init_lock(&writeLock);
+
+
+    omp_lock_t writeLock[numberOfBuckets];
+    for (int i = 0; i < numberOfBuckets; i++)
+        omp_init_lock(&writeLock[i]);
+
     omp_set_num_threads(numberOfThreads);
     
     int bucketRange = rangeOfNumbers/numberOfBuckets;
-    int threadRange = numberOfPoints/numberOfThreads;
+    
+    int chunkSize;
+    
+    if(string(argv[6])=="no")
+        chunkSize = numberOfPoints/numberOfThreads;
+    else{
+        chunkSize = atoi(argv[6]);
+    }
+
     int step = numberOfBuckets/numberOfThreads;
 
     double timeExecStart = omp_get_wtime();
 
-    #pragma omp parallel shared(buckets) 
-    #pragma omp for schedule(static,threadRange)
+    #pragma omp parallel shared(writeLock) 
+    #pragma omp for schedule(static,chunkSize)
     for(int i=0;i<points;i++) 
     {   
+        int idOfBucket;
+
         if( elements[i] == rangeOfNumbers || numberOfBuckets==1){
-            omp_set_lock(&writeLock);
-            buckets[numberOfBuckets-1]->push_back(elements[i]);  
-            omp_unset_lock(&writeLock); 
+            idOfBucket = numberOfBuckets-1;
         }
         else{
-            int idOfBucket = (elements[i]/bucketRange);
-
-            if(idOfBucket>=numberOfBuckets){
-                omp_set_lock(&writeLock);
-                buckets[numberOfBuckets-1]->push_back(elements[i]);  
-                omp_unset_lock(&writeLock);  
-            }
-            else{
-                omp_set_lock(&writeLock);
-                buckets[idOfBucket]->push_back(elements[i]);  
-                omp_unset_lock(&writeLock);
-            }
+            idOfBucket = (elements[i]/bucketRange);
+            if(idOfBucket>=numberOfBuckets) idOfBucket = numberOfBuckets-1;  
         }
+//        cout<<"TID: "<<omp_get_thread_num()<<" rezerwuje sobie dostep do kubelka: "<<idOfBucket<<" dla i = !"<<i<< endl;
+//        omp_set_lock(&writeLock[idOfBucket]);
+        buckets[idOfBucket]->push_back(elements[i]);
+//        omp_unset_lock(&writeLock[idOfBucket]); 
     }
     
     
-    #pragma omp parallel for
-    for(int i=omp_get_thread_num()*step;i<(omp_get_thread_num()*step+step);i++)
+    #pragma omp parallel for schedule(static,step)
+    for(int i=0;i<numberOfBuckets;i++)
         sort(buckets[i]->begin(), buckets[i]->end()); 
     
     double timeExecStop = stopTimer(timeExecStart);
-
+    
     vector<int> sortedData;
     sortedData.reserve(numberOfPoints);
     
     for(int t=0;t<numberOfBuckets;t++)
         copy(buckets[t]->begin(), buckets[t]->end(),back_inserter(sortedData));
 
-    cout<<numberOfPoints<<"     "<<numberOfBuckets<<"    "<<rangeOfNumbers<<"    "<<numberOfThreads<<"    "<<timeExecStop<<endl;
+    cout<<numberOfPoints<<","<<numberOfBuckets<<","<<rangeOfNumbers<<","<<numberOfThreads<<","<<timeExecStop<<endl;
     
     delete[] elements;
-    omp_destroy_lock(&writeLock);
+    
+    for (int i = 0; i < numberOfBuckets; i++)
+        omp_destroy_lock(&writeLock[i]);
 }
